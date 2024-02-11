e815a051e7510d6258f63a473074cf24f95a7eb8
Tazim381 <jucse28.381@gmail.com>
2024-02-11 18:36:57 +0600

    database added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const PORT = 5000
+const connectDB = require("./config/db");
+
 
 app.listen(PORT,(req,res)=>{
     console.log(`App is running on port ${PORT}`)
 })
+
+connectDB();
+
+app.use(express.json());
