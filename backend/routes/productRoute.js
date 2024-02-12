178cd4e687f55d8d6fc1b94ba96bebb7da863aa0
Tazim381 <jucse28.381@gmail.com>
2024-02-12 22:13:08 +0600

    KISS builded

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,10 @@
-const {setProducts,getProducts,deleteProducts,updateProducts} = require('../controllers/productController')
-const router = require('express').Router();
+const {updateProducts} = require('../controllers/productController/updateProducts')
+const {getProducts} = require('../controllers/productController/getProducts')
+const {deleteProducts} = require('../controllers/productController/deleteProducts')
+const {setProducts} = require('../controllers/productController/setProducts')
 
+
+const router = require('express').Router();
 
 router.post("/",setProducts)
 router.get("/",getProducts)
