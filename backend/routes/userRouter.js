c796b8788f3b97ddb73868156bdef7f8919d65fa
Tazim381 <jucse28.381@gmail.com>
2024-02-13 19:15:38 +0600

    user create,delete,update api added

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,7 @@
 const {createUsers} = require('../controllers/userController/createUsers')
 const {deleteUsers} = require('../controllers/userController/deleteUsers')
 const { showAllUsers } = require('../controllers/userController/showAllUsers')
+const { updateUsers } = require('../controllers/userController/updateUsers')
 
 
 const router = require('express').Router()
@@ -8,5 +9,6 @@ const router = require('express').Router()
 router.post('/',createUsers)
 router.delete('/:id',deleteUsers)
 router.get("/",showAllUsers)
+router.put("/:id",updateUsers)
 
 module.exports = router
