c79bac22806a29133cf1abf96ac0102288307e7c
nn-fiha <jucse28.361@gmail.com>
2024-02-13 19:44:37 +0600

    adminDashboard documentation done

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -1,7 +1,21 @@
-const {adminDashboard } = require('../controllers/adminController/adminDashboard')
+/**
+ * Module for handling admin-related routes.
+ * @module adminRouter
+ */
 
+const { adminDashboard } = require('../controllers/adminController/adminDashboard');
 const router = require('express').Router();
 
-router.get("/",adminDashboard)
+/**
+ * Route for accessing the admin dashboard.
+ * @name GET/admin
+ * @function
+ * @memberof module:adminRouter
+ * @inner
+ * @param {string} path - Express route path.
+ * @param {Function} middleware - Middleware function to handle the request.
+ * @returns {Object} Express router object.
+ */
+router.get("/", adminDashboard);
 
-module.exports = router
+module.exports = router;
