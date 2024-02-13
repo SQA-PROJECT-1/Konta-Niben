803b5d5238a8948225327860d8344d2f215877f9
SM2142 <serajummonira2142@gmail.com>
2024-02-13 21:33:24 +0600

    documentation added

--- backend/controllers/productController/addToCart.js
+++ backend/controllers/productController/addToCart.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const User = require("../../models/userModel");
 
-const addToCart = async(req, res) => {
+/**
+ * Add a product to the user's cart.
+ * @param {Object} req - The Express request object representing the HTTP request..
+ * @param {Object} res - The Express response object representing the HTTP response.
+ * @returns {Promise<void>} - A Promise that resolves when the product is successfully added to the user's cart.
+ * @throws {Error} If there is an error while adding the product to the cart.
+ */
+const addToCart = async (req, res) => {
     try {
         const { userId, productId, productName, productCategory, productPrice } = req.body;
 
@@ -14,7 +21,7 @@ const addToCart = async(req, res) => {
         await user.save();
 
         res.status(200).send({ success: true, msg: "Product added to cart successfully" });
-    } catch(error) {
+    } catch (error) {
         res.status(400).send({ success: false, msg: error.message });
     }
 };
