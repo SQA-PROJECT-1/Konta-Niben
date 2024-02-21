27f5d4d0a2a6e9163aab3fa37f42ad26c19fc996
btbsakib <btbsakibm@barytech.com>
2024-02-21 20:56:57 +0600

    get all review api is complete

--- backend/routes/reviewSystemRoute.js
+++ backend/routes/reviewSystemRoute.js
@@ -5,7 +5,7 @@ const addReview = require('../controllers/ReviewController/addReview');
 
 // Routes for adding or updating reviews
 router.post('/addOrUpdate', addReview.addOrUpdateReview);
-
+router.get('/review', addReview.getAllReviewsForProduct);
 
 
 module.exports = router;
