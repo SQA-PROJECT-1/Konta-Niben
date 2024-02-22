2942cc25148a68d90c7581730fcaa0243e4b0789
Tazim381 <jucse28.381@gmail.com>
2024-02-22 22:34:13 +0600

    documentation created for user profile

--- backend/controllers/userController/userProfile.js
+++ backend/controllers/userController/userProfile.js
@@ -1,14 +1,25 @@
-const User = require('../../models/userModel')
+/**
+ * Retrieves the profile of the currently logged-in user.
+ * @async
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} The profile of the logged-in user.
+ */
+const userProfile = async (req, res) => {
+  try {
+      // Get the email of the logged-in user from the request object
+      const email = req.user.userEmail;
 
-const userProfile = async(req,res)=>{
-    try {
-        const email = req.user.userEmail
-        const admin = await User.findOne({useEmail:email});
-        res.status(200).json(admin);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Something went wrong to get admin" });
-      }
+      // Find the user in the database based on the email
+      const admin = await User.findOne({ userEmail: email });
+
+      // Send the user profile in the response
+      res.status(200).json(admin);
+  } catch (error) {
+      // Handle errors
+      console.error(error);
+      res.status(500).json({ message: "Something went wrong while retrieving the user profile." });
+  }
 }
 
-module.exports = userProfile
+module.exports = userProfile;
