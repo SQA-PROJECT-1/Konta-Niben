cb65a231f24692db5840db6f36427b154fcdd1c1
Tazim381 <jucse28.381@gmail.com>
2024-02-24 18:02:33 +0600

    2nd test case implemented

--- backend/controllers/userController/showAllUsers.test.js
+++ backend/controllers/userController/showAllUsers.test.js
@@ -27,4 +27,24 @@ describe('showAllUsers function', () => {
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith(dummyUsers);
   });
+
+  test('should return "User not found" if no users except admin are found', async () => {
+    // Mock the behavior of User.find to return empty array
+    User.find.mockResolvedValue([]);
+
+    // Mock request and response objects
+    const req = {};
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await showAllUsers(req, res);
+
+    // Expectations
+    expect(User.find).toHaveBeenCalledWith({ userRole: { $ne: 'admin' } });
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith('User not found');
+  });
+  
 })
