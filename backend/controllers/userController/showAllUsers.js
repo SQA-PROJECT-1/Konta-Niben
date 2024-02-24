9d4e9d94f94cf3cad8077a9a818f879a5616ebaa
Tazim381 <jucse28.381@gmail.com>
2024-02-24 17:56:32 +0600

    implementing showAllUsers using TDD

--- backend/controllers/userController/showAllUsers.js
+++ backend/controllers/userController/showAllUsers.js
@@ -12,10 +12,6 @@ const User = require('../../models/userModel')
 const showAllUsers = async(req,res)=>{
     try{
       const allUsers = await User.find({ userRole: { $ne: 'admin' } });
-      if(!allUsers || allUsers.length==0){
-        return res.status(400).json("User not found")
-      }
-      res.status(200).json(allUsers)
     }
     catch(error) {
         res.status(500).json(error)
