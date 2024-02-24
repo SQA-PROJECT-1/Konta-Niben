b4ee3e6da6c91144e21f3b0665399a8c7cb78d7f
Tazim381 <jucse28.381@gmail.com>
2024-02-24 18:15:47 +0600

    write code according to test case 3

--- backend/controllers/userController/showAllUsers.js
+++ backend/controllers/userController/showAllUsers.js
@@ -18,7 +18,7 @@ const showAllUsers = async(req,res)=>{
       res.status(200).json(allUsers)
     }
     catch(error) {
-        res.status(500).json(error)
+        res.status(500).json(error.message)
     }
 }
 
