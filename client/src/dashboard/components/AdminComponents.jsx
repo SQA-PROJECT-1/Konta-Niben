90d02229d25bc90c4a80453510caf6cb22fc6066
Tazim381 <jucse28.381@gmail.com>
2024-02-26 21:30:39 +0600

    admin email changed

--- client/src/dashboard/components/AdminComponents.jsx
+++ client/src/dashboard/components/AdminComponents.jsx
@@ -24,14 +24,14 @@ const AdminComponents = ({user}) => {
   }
   return (
     <div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <img src="https://th.bing.com/th/id/R.857b26dafe5b74439d5fcfc23b77aa1a?rik=cZdp5iaYNAxWTA&riu=http%3a%2f%2fwww.liveadmins.com%2fwp-content%2fuploads%2f2015%2f09%2fProduct-Launch.jpg&ehk=tqhzE2p1F9XQzyKdEXTPZHADJpLq8vcw3fLXgv25J%2bE%3d&risl=&pid=ImgRaw&r=0" />
+    <img src="https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg"/>
     <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">{user.userName}</div>
         <p class="text-gray-700 text-base">
             Name: {user.userName}
         </p>
         <p class="text-gray-700 text-base">
-            Email: {user.userId}
+            Email: {user.userEmail}
         </p>
         <button onClick={()=>handleClick(user.userId)} className='bg-slate-500 text-white px-3 py-2 rounded-lg mt-5'>Remove From Admin</button>
     </div>
--- client/src/dashboard/components/UserComponents.jsx
+++ client/src/dashboard/components/UserComponents.jsx
@@ -43,7 +43,7 @@ const UserComponents = ({user}) => {
   }
   return (
     <div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <img src="https://th.bing.com/th/id/R.857b26dafe5b74439d5fcfc23b77aa1a?rik=cZdp5iaYNAxWTA&riu=http%3a%2f%2fwww.liveadmins.com%2fwp-content%2fuploads%2f2015%2f09%2fProduct-Launch.jpg&ehk=tqhzE2p1F9XQzyKdEXTPZHADJpLq8vcw3fLXgv25J%2bE%3d&risl=&pid=ImgRaw&r=0" />
+    <img src="https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg" />
     <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">{user.userName}</div>
         <p class="text-gray-700 text-base">
