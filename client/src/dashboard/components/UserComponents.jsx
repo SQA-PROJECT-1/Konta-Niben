e5ceb86d86d7228aff9cfb8eba938b08b4a95be4
Tazim381 <jucse28.381@gmail.com>
2024-02-26 20:57:24 +0600

    user delete Ui completed

--- client/src/dashboard/components/UserComponents.jsx
+++ client/src/dashboard/components/UserComponents.jsx
@@ -22,6 +22,25 @@ const UserComponents = ({user}) => {
     });
 
   }
+
+  const handleDelete=(id)=>{
+    axios.delete(`http://localhost:5000/api/users/${id}`,{
+      userRole:"admin"
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        alert("User removed Successfully");
+        windows.location.reload()
+      } else {
+        alert("User not deleted!");
+      }
+      //console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  }
   return (
     <div class="max-w-sm rounded overflow-hidden shadow-lg">
     <img src="https://th.bing.com/th/id/R.857b26dafe5b74439d5fcfc23b77aa1a?rik=cZdp5iaYNAxWTA&riu=http%3a%2f%2fwww.liveadmins.com%2fwp-content%2fuploads%2f2015%2f09%2fProduct-Launch.jpg&ehk=tqhzE2p1F9XQzyKdEXTPZHADJpLq8vcw3fLXgv25J%2bE%3d&risl=&pid=ImgRaw&r=0" />
@@ -31,9 +50,15 @@ const UserComponents = ({user}) => {
             Name: {user.userName}
         </p>
         <p class="text-gray-700 text-base">
-            Email: {user.userEamil}
+            Email: {user.userEmail}
         </p>
-        <button onClick={()=>handleClick(user.userId)} className='bg-slate-500 text-white px-3 py-2 rounded-lg mt-5'>Make Admin</button>
+        <div className='flex gap-5'>
+        <div>
+        <button onClick={()=>handleClick(user.userId)} className='bg-slate-500 text-white px-3 py-2 rounded-lg mt-5'>Make Admin</button></div>
+        <div>
+        <button onClick={()=>handleDelete(user.userId)} className='bg-slate-500 text-white px-3 py-2 rounded-lg mt-5'>Delete User</button>
+        </div>
+        </div>
     </div>
 </div>
   )
